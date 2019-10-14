f06cbff51fcd8f097cf150943b1920148f332802
liuwanyong2017 <2674706698@qq.com>
2019-10-14 18:44:50 +0800

    模拟web容器的基础原理，路由加fs读取数据，然后发送。获取路由，然后控制路由，分化逻辑处理，最后发送。只是实现了静态资源的死板地交互了，可扩展性很差

---
+++ http/webBox.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const url = require('url')
+
+const  fs = require('fs')
+const path = require('path')
+//对web容器的模拟，就是对路由和读取写入数据文件的逻辑的结合
+
+http.createServer((req,res)=>{
+    //拿到路由
+    console.log(req.url);
+    // const url = new URL(req.url)  会报错，因为 req.url  = '/'
+    const urlObj = url.parse(req.url)
+    const {pathname} = urlObj
+    console.log(pathname);
+    //路由处理，管理
+    if(pathname === '/'){
+        //读取数据，发送数据
+        fs.readFile(path.join(__dirname,'../webBoxSource/index.html'),(err,data)=>{
+            if (err ) throw err;
+            res.end(data);
+            console.log(data);
+        })
+    }else {
+        //读取数据，发送数据
+        fs.readFile(path.join(__dirname,`../webBoxSource/${pathname}`),(err,data)=>{
+            if (err ) throw err;
+            res.end(data);
+            console.log(data);
+        })
+    }
+    //但是局限性在于，仅仅是静态文件的获取了。很死板，而且可扩展性太差了
+
+}).listen(3000,'localhost');
+
+
