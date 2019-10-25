9f8d6aabe7e83288fcb3b4385cae8c046ff3b647
liuwanyong2017 <2674706698@qq.com>
2019-10-25 17:48:58 +0800

    引入nunjucks模板，并且设置模板引擎的文件路径，同时设置模板开发环境下不缓存

--- project/src/app.js
+++ project/src/app.js
@@ -1,10 +1,19 @@
 import express from "express";
 import configs from "../config";
+import nunjucks from "nunjucks";
 
 const app = express();
 
 //静态文件的路由设置
 app.use(express.static(configs.publicPath));
+
+
+//模板引擎的中间键的配置
+nunjucks.configure(configs.viewPath, {
+    autoescape: true,
+    express: app,
+    noCache:true     //开发阶段模板不用缓存的设置，记得上线时去掉
+});
 
 app.listen(3000, () => {
     console.log("listening");
