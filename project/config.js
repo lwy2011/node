011213dc2dd05c1c29a69b386e3c7634ba1f0375
liuwanyong2017 <2674706698@qq.com>
2019-10-25 17:18:06 +0800

    设置静态文件路由，静态文件的路径配置

---
+++ project/config.js
@@ -0,0 +1,7 @@
+import {join} from 'path'
+export default {
+    viewPath : join(__dirname,'./views'),
+    publicPath:join(__dirname,'./public')
+}
+
+//这是设置静态文件路径
--- project/src/app.js
+++ project/src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
+import configs from "../config";
 
 const app = express();
 
+//静态文件的路由设置
+app.use(express.static(configs.publicPath));
 
 app.listen(3000, () => {
     console.log("listening");
