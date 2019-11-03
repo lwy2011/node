c1a1285fd7bbc52089666b8d6fc1243d3c4e4ebb
liuwanyong2017 <2674706698@qq.com>
2019-11-04 00:05:56 +0800

    分页的功能实现了！

--- project/router/source.js
+++ project/router/source.js
@@ -3,21 +3,45 @@ import Source from "../model/source";
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    Source.find({}, (e, lists) => {
-        if (e) return next(e);
-        res.render("back/source.html", {lists});
-    });
-});
+// router.get("/", (req, res, next) => {
+//     Source.find({}, (e, lists) => {
+//         if (e) return next(e);
+//         res.render("back/source.html", {lists});
+//     });
+// });
 router.get("/add", (req, res) => {
     res.render("back/source.add.html");
 });
 router.get("/edit/:id", (req, res, next) => {
     Source.findById(req.params.id, (e, data) => {
         if (e) return next(e);
-        res.render("back/source.edit.html",{data});
+        res.render("back/source.edit.html", {data});
     });
 });
 
+router.get("/", (req, res) => {
+    let {count, page} = req.query;
+    count = +req.query.count || 2;
+    page = +req.query.page || 1;
+    Source.find().skip((page - 1) * count).limit(count).exec(
+        (e, lists) => {
+            if (e) return e;
+            Source.countDocuments(
+                (e1, c) => {
+                    if (e1) return e1;
+                    res.render("back/source.html",
+                        {
+                            lists,
+                            totalPage: Math.ceil(c / count),
+                            page,
+                            count
+                        }
+                    );
+                }
+            );
+
+        }
+    );
+});
 
 export default router;
