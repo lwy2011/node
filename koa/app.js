9e557e7becd267d808ed4435607db95729b7fd8e
liuwanyong2017 <2674706698@qq.com>
2019-12-23 19:20:38 +0800

    引入koa-router,简单使用

--- koa/app.js
+++ koa/app.js
@@ -1,4 +1,5 @@
 import Koa from "koa";
+import Router from "koa-router";
 
 const app = new Koa();
 
@@ -33,7 +34,6 @@ const app = new Koa();
 // 中间键函数都要用async await 强制做成Promise，又跟next的实质为promise正好匹配。
 
 
-
 //中间键函数用async ，next函数用await：为了保证洋葱模型的顺序执行！
 
 // app.use((ctx, next) => {
@@ -60,30 +60,55 @@ const app = new Koa();
 //await 会阻塞线程，锁定当前的执行顺序。这保证了洋葱模式的执行顺序，不被嵌套的异步所打扰！
 
 
-
 //await 也是适合拿到异步的操作的最终值，而不是一个promise对象,
 // 上面说过，每一个next引导的最终结果都是promise
 
-app.use(async (ctx, next) => {
-    console.log(1);
-    await next();
-    console.log(12);
-    console.log(ctx.r);
+// app.use(async (ctx, next) => {
+//     console.log(1);
+//     await next();
+//     console.log(12);
+//     console.log(ctx.r);
+// });
+//
+// app.use(async (ctx, next) => {
+//     console.log(2);
+//     ctx.r = await next();
+//     console.log(22);
+// });
+//
+// app.use( async () => {
+//     return await new Promise((resolve) => {
+//         setTimeout(() => {
+//             console.log(21);
+//             resolve(3);
+//         }, 1200);
+//     })
+// });
+
+// 1
+// 2
+// 21    阻塞
+// 22   阻塞
+// 12
+// 3
+
+// 路由简单引入
+// app.use((ctx, next) => {
+//     const {path, method} = ctx;
+//     console.log(path, method);
+//     if (path === "/" && method === "GET") {   //路由的最基础的演化，太麻烦了
+//         ctx.body = {res: "host"};
+//     }
+// });
+
+
+//koa-router:
+const router = new Router();
+router.get("/", (ctx, next) => {
+    ctx.body = {res:'hahha'}
 });
 
-app.use(async (ctx, next) => {
-    console.log(2);
-    ctx.r = await next();
-    console.log(22);
-});
 
-app.use( async () => {
-    return await new Promise((resolve) => {
-        setTimeout(() => {
-            console.log(21);
-            resolve(3);
-        }, 1200);
-    })
-});
+app.use(router.routes());
 
 app.listen(3000);
