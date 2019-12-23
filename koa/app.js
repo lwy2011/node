6ab40791062862cbd8b6893e4d4a7c56bca36f73
liuwanyong2017 <2674706698@qq.com>
2019-12-23 14:42:13 +0800

    中间键的next，调用，洋葱模型演示，先进后出，后进先出！

--- koa/app.js
+++ koa/app.js
@@ -1,3 +1,27 @@
 import {test} from "./router";
+import Koa from "koa";
 
-test();
+const app = new Koa();
+
+//洋葱模型演示：
+app.use((ctx, next) => {
+    console.log(1);
+    next();  //下一个中间键，否则就不会到下一个中间键
+    console.log(12);
+});
+app.use((ctx, next) => {
+    console.log(2);
+    next();
+    console.log(22);
+});
+// 1
+// 2
+// test
+// 22
+// 12
+
+
+
+app.use(test);
+
+app.listen(3000);
