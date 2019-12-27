f537857c4ab0f70a2671031dcbd03a54ebeeef9f
liuwanyong2017 <2674706698@qq.com>
2019-12-27 16:22:05 +0800

    用bcrypt加密密码，成功

--- koa/app/router/v1/user.js
+++ koa/app/router/v1/user.js
@@ -1,4 +1,5 @@
 import Router from "koa-router";
+import bcrypt from "bcrypt";
 import RegisterValidator from "../../validators/register";
 import User from "../../model/user";
 
@@ -14,14 +15,15 @@ router.post("/register", async (ctx) => {
     //nickname password1  password2  email
 
     const v = await new RegisterValidator().validate(ctx);
-
+    const salt = bcrypt.genSaltSync(10);   //10是计算的成本复杂设置数，一般般就可以了
+    const pwd = bcrypt.hashSync(v.get("body.password1"),salt);
     const user = {
         nickname: v.get("body.nickname"),
-        password: v.get("body.password"),
+        password: pwd,
         email: v.get("body.email")
     };
     await User.create(user);
-    console.log(12,User);
+    // console.log(12, User);
 
 
 });
