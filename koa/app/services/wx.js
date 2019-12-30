7a2ba2809ec46082c32f5d1816b73d01c8d590e0
liuwanyong2017 <2674706698@qq.com>
2019-12-30 17:18:13 +0800

    配置小程序的id,secrit,添加axios，发送请求给微信后台，携带小程序的ID，secretid,前端传来的code。拿到openID

--- koa/app/services/wx.js
+++ koa/app/services/wx.js
@@ -1,19 +1,33 @@
 //也是业务层，等同于model文件夹里的其他业务，为了展示业务分层，
 // 做了services层
 
+import config from "../../config";
+import util from "util";
+import axios from "axios";
+import {AuthFailed} from "../../core/http-exception";
+
 class Wx {
-    codeToToken(code){
+    async codeToToken(code) {
         //登录原理：
         //前端生成和发送code，接受到code，
         // 后端把code , appid , appsecret ,code 发送给wechat http API
-    //微信服务器返回了session_key ,openid等
-    //     自定义登录态跟openid ,session_key关联。
+        //微信服务器返回了session_key ,openid等
+        //     自定义登录态跟openid ,session_key关联。
 
-    // 不需要注册，openid是唯一标识
-    //     appid appsecret小程序申请的
+        // 不需要注册，openid是唯一标识
+        //     appid appsecret小程序申请的
+        const {appUrl, appId, appSecret} = config.wx;
+        const url = util.format(appUrl, appId, appSecret, code);
+        const res = await axios.get(url);
+        if (res.status !== 200) {
+            throw new AuthFailed("openid获取失败！");
+        }
+
+        if (res.errcode !== 0) {
+            throw new AuthFailed(res.errmsg, res.errcode);
+        }
     }
 }
 
 
-
-export default Wx
+export default Wx;
--- koa/config.js
+++ koa/config.js
@@ -10,6 +10,12 @@ const config = {
     security:{
         secretKey:'sjf$nweis2331mk',
         expiresIn:60*60  //单位为分钟，jsonwebtoken的生态
+    },
+    wx:{
+        appId:'自己申请的app给的',
+        appSecret :'也是申请app给的！',
+        appUrl : `https://api.weixin.qq.com/sns/jscode2session?
+        appid=%s&secret=%s&js_code=%s&grant_type=authorization_code`
     }
 };
 
