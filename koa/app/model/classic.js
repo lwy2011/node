4c87bfc5f83f1cbc72578769b043ba92111300e1
liuwanyong2017 <2674706698@qq.com>
2020-01-03 10:50:28 +0800

    创建movie,misic,sentence的数据表模型

---
+++ koa/app/model/classic.js
@@ -0,0 +1,50 @@
+//movie  sentence music 三个分类
+
+
+//classic 是三个类的共性类，大类
+// 尽可能地找到贴切的单词来描述
+
+//图片，标题，日期，内容，点赞数量，image ,title ,pubdate,content ,fav_nums,type是类别
+
+//music url
+
+
+import Sequelize, {Model} from "sequelize";
+
+
+const classicFields = {
+    image: Sequelize.STRING,
+    title: Sequelize.STRING,
+    content: Sequelize.STRING,
+    pubdate: Sequelize.DATEONLY,
+    fav_nums: Sequelize.INTEGER,
+    type: Sequelize.TINYINT
+};
+
+class Movie extends Model {
+
+}
+
+
+Movie.init(classicFields,{sequelize,tableName:'movie'});
+
+class Sentence extends Model {
+
+}
+
+
+Sentence.init(classicFields,{sequelize,tableName:'sentence'});
+
+
+class Music extends Model {
+
+}
+
+
+Music.init(
+    {...classicFields,url:Sequelize.STRING},
+    {sequelize,tableName:'movie'}
+    );
+
+
+export {Movie,Music,Sentence}
